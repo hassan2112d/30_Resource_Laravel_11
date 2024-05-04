bbb21ff6c38e14a177c118bb25d97a10424f8847
hassan2112d <hassan2112d@aptechgdn.net>
2024-05-04 13:40:08 +0500

    Resource Finished

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\Usercontroller;
+use Egulias\EmailValidator\Parser\Comment;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+//Single Controller
 Route::resource('user',Usercontroller::class);
+
+//Multiple Controller
+
+// Route::resource([
+//     'users' => Usercontroller::class,
+//     'comment' => CommentController::class
+// ]);
+
+Route::resource('user.comment',CommentController::class);
+
+
+
